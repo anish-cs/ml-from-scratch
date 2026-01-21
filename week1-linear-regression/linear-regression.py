b612f03b67c229a2688d4feaacb6539546f19a5a
anish-cs <anish.nair499@gmail.com>
2026-01-21 10:12:20 -0500

    Implemented fit and tested with values. Impleneted R^2 to see if fit is accurate.

--- week1-linear-regression/linear-regression.py
+++ week1-linear-regression/linear-regression.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 class LinearRegression:
     def __init__(self, learning_rate = 0.01, n=1000):
         self.lr = learning_rate
@@ -9,13 +8,48 @@ class LinearRegression:
         self.bias = None
         self.losses = []
     def predict(self, X):
-
         return X @ self.weights + self.bias
-    
+    def fit (self, X, y):
+        self.bias = 0
+        n_samp, n_feat = X.shape
+        self.weights = np.zeros(n_feat)
+        for i in range(self.n):
+            y_pred = self.predict(X)
+            loss = np.mean((y_pred-y)**2)
+            self.losses.append(loss)
+
+            dw = (2/n_samp) * X.T @ (y_pred - y) #take partial derivatives of loss function
+            db = (2/n_samp) * np.sum(y_pred - y) 
+
+            self.weights -= self.lr * dw #negative so it converges.
+
+            self.bias -= self.lr * db
+        
+    def find_R2(self, w, b):   # R2 = (SSM - SSF)/SSM; trying to implement p value soon for better accuracy
+        y_fit = X @ self.weights + self.bias
+        SSF = np.sum((y-y_fit)**2)
+        SSM = np.sum((y-np.mean(y))**2)
+        R2 = 1 - SSF/SSM
+        return R2 * 100
+
+
 
 if __name__ == "__main__":
+    X = np.array([[1],[2],[3],[4]], dtype=float)
+    y = 2 * X.flatten() + 3
+
     model = LinearRegression()
-    model.weights =np.array([2])
-    model.bias = 3.0
-    X_test = np.array([[1],[2],[3]])
-    print('Test predictions:', model.predict(X_test))
+    model.fit(X,y)
+    print("Learned weight:", model.weights)
+    print("Learned bias:", model.bias)
+    print("R square value", model.find_R2(model.weights, model.bias))
+    plt.scatter(X,y)
+
+    X_line = np.linspace(X.min(), X.max(), 100).reshape(-1, 1)
+    y_line = model.predict(X_line)
+    plt.plot(X_line, y_line)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Linear Regression Fit")
+    plt.show()
+
